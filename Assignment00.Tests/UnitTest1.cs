71647aacbb0ea8cff925973799c6e7d34158a85e
death <deth@itu.dk>
2021-09-04 20:18:24 +0200

    Tested and implemented IsLeapYear.

--- Assignment00.Tests/UnitTest1.cs
+++ Assignment00.Tests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace Assignment00.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Main_Prints_HelloWorld()
         {
           var writer = new StringWriter();
           Console.SetOut(writer);
@@ -18,6 +18,15 @@ namespace Assignment00.Tests
           var output = writer.GetStringBuilder().ToString().Trim();
           Assert.Equal("Hello World!", output);
           
+        }
+
+        [Fact]
+        public void IsLeapYear_Gets_LeapYears_Correct() {
+            Assert.True(Program.IsLeapYear(1600));
+            Assert.True(Program.IsLeapYear(2000));
+            Assert.False(Program.IsLeapYear(1700));
+            Assert.False(Program.IsLeapYear(1800));
+            Assert.False(Program.IsLeapYear(1900));
         }
     }
 }
--- Assignment00/Program.cs
+++ Assignment00/Program.cs
@@ -7,6 +7,22 @@ namespace Assignment00
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+        }
+
+        public static bool IsLeapYear(int year) 
+        {
+            if (year % 4 == 0)  {
+                if(year % 100 != 0 || year % 400 == 0) {
+                   return true; 
+                }
+                else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
         }
     }
 }
+//Run by: dotnet run -p .\Assignment00\ 
