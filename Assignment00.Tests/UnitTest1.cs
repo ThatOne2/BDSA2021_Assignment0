94977d4d96e2a42412e01698694632657f608a44
death <deth@itu.dk>
2021-09-04 16:58:28 +0200

    HelloWorld moved to main for debugging & now works

---
+++ Assignment00.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace Assignment.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+          var writer = new StringWriter();
+          Console.SetOut(writer);
+
+          Program.Main(new string[0]);
+
+          var output = writer.GetStringBuilder().ToString.Trim();
+          Assert.Equal("Hello World!", output);
+          
+        }
+    }
+}
+
